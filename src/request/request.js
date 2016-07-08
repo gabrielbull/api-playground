072f8e636b833572dc9b60ef4e70578772f914f9
Gabriel Bull <me@gabrielbull.com>
2016-07-08 14:00:46 -0400

    Added minimize/maximize to requests

--- src/request/request.js
+++ src/request/request.js
@@ -13,7 +13,18 @@ const styles = {
     borderTop: '1px solid #222'
   },
 
+  header: {
+    display: 'flex'
+  },
+
+  actions: {
+    marginLeft: 'auto'
+  },
+
   name: {
+    cursor: 'pointer',
+    WebkitUserSelect: 'none',
+    userSelect: 'none',
     display: 'inline-block',
     width: '200px'
   },
@@ -23,8 +34,14 @@ const styles = {
     marginTop: '20px'
   },
 
+  reset: {
+    color: '#ff3900',
+    cursor: 'pointer'
+  },
+
   run: {
     color: '#a8ff00',
+    marginLeft: '20px',
     cursor: 'pointer'
   },
 
@@ -66,7 +83,8 @@ class Request extends Component {
       method: localStorage[this.persistKey + '.method'],
       url: localStorage[this.persistKey + '.url'],
       showLoader: false,
-      rows: []
+      rows: [],
+      isMinified: localStorage[this.persistKey + '.isMinified'] === true
     };
     if (localStorage[this.persistKey + '.rows']) {
       try {
@@ -78,6 +96,16 @@ class Request extends Component {
     }
   }
 
+  minify = () => {
+    this.setState({ isMinified: true });
+    localStorage[this.persistKey + '.isMinified'] = true;
+  };
+
+  maximize = () => {
+    this.setState({ isMinified: false });
+    localStorage[this.persistKey + '.isMinified'] = false;
+  };
+
   updateMethod = method => {
     localStorage[this.persistKey + '.method'] = method;
     this.setState({ method });
@@ -86,6 +114,11 @@ class Request extends Component {
   updateUrl = url => {
     localStorage[this.persistKey + '.url'] = url;
     this.setState({ url });
+  };
+
+  reset = () => {
+    this.setState({ showLoader: false, rows: [] });
+    localStorage[this.persistKey + '.rows'] = JSON.stringify([]);
   };
 
   request = () => {
@@ -180,7 +213,7 @@ class Request extends Component {
     this.setState({ rows });
     rows = [ row, ...this.state.rows ];
     localStorage[this.persistKey + '.rows'] = JSON.stringify(rows);
-    setTimeout(() => this.setState({ rows }), 250);
+    setTimeout(() => this.setState({ rows }), 10);
   };
 
   getData() {
@@ -210,25 +243,60 @@ class Request extends Component {
         </div>
       );
     }
-
+    if (this.state.isMinified) {
+      return (
+        <div style={styles.container}>
+          <span style={styles.name} onClick={this.maximize}>
+            <svg x="0px" y="0px" width="10px" height="10px" viewBox="0 0 10 10" style={{ marginRight: '4px' }}>
+              <path  fill="#ffffff" d="M6,6l3.5,0C9.8,6,10,5.9,10,5.6l0-1.2C10,4.1,9.8,4,9.6,4L6,4
+                l0-3.5C6,0.2,5.9,0,5.6,0L4.4,0C4.1,0,4,0.2,4,0.4L4,4L0.4,4C0.2,4,0,4.1,0,4.4l0,1.2C0,5.9,0.2,6,0.4,6L4,6l0,3.5
+                C4,9.8,4.1,10,4.4,10l1.3,0C5.9,10,6,9.8,6,9.6L6,6z"/>
+            </svg>
+            {this.props.name}
+          </span>
+        </div>
+      );
+    }
     return (
       <div style={styles.container}>
-        <span style={styles.name}>
-          {this.props.name}
-        </span>
-        <a onClick={this.request} style={styles.run}>
-          Run
-          <svg x="0px" y="0px" width="5px" height="10px" viewBox="0 0 5 10" style={{ marginLeft: '4px' }}>
-            <polygon fill="#a8ff00" points="0,0 0,10 5,5 "/>
-          </svg>
-        </a>
-        <Loader size="12" style={{ marginLeft: '10px', opacity: this.state.showLoader ? 1 : 0 }}/>
-        <span style={styles.method}>
-          {this.state.method}
-        </span>
-        <span style={styles.url}>
-          {url}
-        </span>
+        <div style={styles.header}>
+          <span style={styles.name} onClick={this.minify}>
+            <svg x="0px" y="0px" width="10px" height="10px" viewBox="0 0 10 2.1" style={{ marginRight: '4px' }}>
+              <path
+                fill="#ffffff"
+                d="M9.6,2.1c0.2,0,0.4-0.2,0.4-0.4l0-1.2C10,0.2,9.8,0,9.6,0L0.4,0
+                C0.2,0,0,0.2,0,0.4l0,1.2c0,0.2,0.2,0.4,0.4,0.4L9.6,2.1z"
+              />
+            </svg>
+            {this.props.name}
+          </span>
+          <Loader size="12" style={{ marginLeft: '10px', opacity: this.state.showLoader ? 1 : 0 }}/>
+          <span style={styles.method}>
+            {this.state.method}
+          </span>
+          <span style={styles.url}>
+            {url}
+          </span>
+          <div style={styles.actions}>
+            <a onClick={this.reset} style={styles.reset}>
+              Reset
+              <svg x="0px" y="0px" width="10px" height="10px" viewBox="0 0 10 10" style={{ marginLeft: '4px' }}>
+                <path
+                  fill="#ff3900"
+                  d="M6.8,5l3.1-3.1c0.2-0.2,0.2-0.5,0-0.7L8.8,0.1C8.6,0,8.3,0,8.1,0.1
+                  L5,3.2L1.9,0.1C1.7,0,1.4,0,1.2,0.1L0.1,1.2C0,1.4,0,1.7,0.1,1.9L3.2,5L0.1,8.1C0,8.3,0,8.6,0.1,8.8l1.1,1.1c0.2,0.2,0.5,0.2,0.7,0
+                  L5,6.8l3.1,3.1c0.2,0.2,0.5,0.2,0.7,0l1.1-1.1c0.2-0.2,0.2-0.5,0-0.7L6.8,5z"
+                />
+              </svg>
+            </a>
+            <a onClick={this.request} style={styles.run}>
+              Run
+              <svg x="0px" y="0px" width="5px" height="10px" viewBox="0 0 5 10" style={{ marginLeft: '4px' }}>
+                <polygon fill="#a8ff00" points="0,0 0,10 5,5 "/>
+              </svg>
+            </a>
+          </div>
+        </div>
         {paramRow}
         <div>
           <Table headers={['Method', 'URL', 'Status', 'Type', 'Time', 'Request Headers', 'Request Content', 'Response Headers', 'Response Content' ]}>
